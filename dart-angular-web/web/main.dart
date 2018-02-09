e4556a80312e01f601c6c3c14ad0e51a063fef7f
Park <neverzero9@gmail.com>
2018-02-09 23:44:32 +0700

    dart-angular-web : update online mode connect with database

--- dart-angular-web/web/main.dart
+++ dart-angular-web/web/main.dart
@@ -1,6 +1,8 @@
 import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 import 'package:dart_angular_web/app_component.dart';
+import 'package:dart_angular_web/in_memory_data_service.dart';
+import 'package:http/browser_client.dart';
 
 // This is main application like java
 void main() {
@@ -8,5 +10,6 @@ void main() {
     ROUTER_PROVIDERS,
     // Remove next line in production
     provide(LocationStrategy, useClass: HashLocationStrategy),
+    provide(BrowserClient, useFactory: () => new BrowserClient(), deps: [])
   ]);
 }
