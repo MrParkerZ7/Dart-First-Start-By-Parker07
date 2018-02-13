db4119f38878541a131a3dbe932392eec383ddc9
Park <neverzero9@gmail.com>
2018-02-14 03:59:19 +0700

    Dart from online course youtube Richard-Kim 01

---
+++ dart-richard-kim-01/web/main.dart
@@ -0,0 +1,21 @@
+import 'package:angular/angular.dart';
+import 'dart:html';
+import 'dart:math';
+
+void main() {
+  ButtonInputElement submit = querySelector('#submit'); // declare value type.
+  NumberInputElement age = querySelector('#age');
+  TextInputElement name = querySelector('#name');
+
+  // Use onClick listen to clean name to empty value.
+  name.onClick.listen((MouseEvent e) => name.value = '');
+
+  age.onChange.listen((Event e) {
+    window.alert("your age = $age");
+  });
+
+  // Use onClick listen to express event window alert.
+  submit.onClick.listen((MouseEvent e) {
+    window.alert("Hi, ${name.value}. you are ${age.value} year old");
+  });
+}
